ad5a2c5418b6128d84872d853b1c36871067f1af
pigalleg <pigalleg@gmail.com>
2016-08-22 19:01:59 -0300

    Added comments to code

--- libraries/Communication/technology.py
+++ libraries/Communication/technology.py
@@ -10,28 +10,34 @@ import socket
 #
 #
 
-
+# Class for TCP socket server connections.
+# It stores multiple sockets and constructs a client list.
+#
+#
+# 
 class WiFi:
 
 	def __init__(self):
 		self.server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)	# after interrumping this scripts sockets are still open due to a time_wait state. This cancels it
 		self.maxConnections = 5  											
-		self.serverAddress=("", 8001)										# defines port
-		self.server.bind(self.serverAddress)  								# binds sockets to a specified address
+		self.serverAddress=("", 8001)										# defines address and port for the client
+		self.server.bind(self.serverAddress)  								# binds sockets to a specified (address,port) couple
 		self.server.listen(self.maxConnections)
 		self.clientList=[]													# list of [socket,ip]. One per ip
 
+	# liten incoming client connections and add them to clientLIst
 	def listenConnection(self):
 		client, clientAddress = self.server.accept() 						# Blocking function. Waits for connections 
-		self.addClient(client,clientAddress[0])					
+		self.addClient(client,clientAddress[0])								# clientAddress=[client_ip, client_port]
 		return True
 
 	def closeConnection(self,socket):
 		socket.close()
 
+	# receives a message from one socket. 
 	def receive(self,socket):														
-		messageLenght =1
+		messageLenght =1													# pre settled lenght
 		chunks = []
 		bytesRecd = 0
 		while bytesRecd< messageLenght:
@@ -51,11 +57,13 @@ class WiFi:
 			totalsent = totalsent + sent
 
 
-	# Cleans			
+	# add pair [socket,ip] to the clientList.
+	# does not control if client steals ip.
+	# different ips have always different sockets: (server_ip, server_port,client_ip,client_port) is unique			
 	def addClient(self, socket, ip):
 		notFound=True
 		for aux in self.clientList:
-			if ip==aux[1]:
+			if ip==aux[1]:													# upgrades socket if it was already 			
 				aux[0]=socket
 				notFound=False
 		if notFound:
@@ -65,3 +73,5 @@ class WiFi:
 
 	def printAddress(self):
 		print self.clientAddress
+
+
